99ac6523d40a2bf37256e98907ad6d84f1bc5d31
Pablo Duarte de Quadros <160532715+PabloDDQ@users.noreply.github.com>
2024-10-27 13:26:21 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -2,44 +2,34 @@ import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String continuar;
 
         do {
             System.out.print("Digite o operador +(Somar), *(Multiplicar), /(Dividir), -(Subtrair), **(Potenciação): ");
-            String operacao_digitada = leitor.nextLine();
+            String operacao_digitada = scanner.nextLine();
 
             System.out.println("Operação escolhida: " + operacao_digitada);
 
-            if (operacao_digitada.equals("+")) {
-                System.out.print("Digite o primeiro número: ");
-                int num1 = leitor.nextInt();
-                System.out.print("Digite o segundo número: ");
-                int num2 = leitor.nextInt();
+            System.out.print("Digite o primeiro número: ");
+            Double num1 = scanner.nextDouble();
+            
+            System.out.print("Digite o segundo número: ");
+            Double num2 = scanner.nextDouble();
 
+            if (operacao_digitada.equals("+")) {
                 System.out.print("O resultado da soma de " + num1 + " e " + num2 + " é de : ");
                 sum(num1, num2);
-            } else if (operacao_digitada.equals("-")) {
-                System.out.print("Digite o primeiro numero: ");
-                int num1 = leitor.nextInt();
-                System.out.print("Digite o segundo numero: ");
-                int num2 = leitor.nextInt();
 
+            } else if (operacao_digitada.equals("-")) {
                 System.out.print("O resultado da subtração de " + num1 + " e " + num2 + " é de : ");
                 sub(num1, num2);
-            } else if (operacao_digitada.equals("*")) {
-                System.out.print("Digite o primeiro numero: ");
-                int num1 = leitor.nextInt();
-                System.out.print("Digite o segundo numero: ");
-                int num2 = leitor.nextInt();
 
+            } else if (operacao_digitada.equals("*")) {
                 System.out.print("O resultado da multiplicação de " + num1 + " e " + num2 + " é de : ");
                 mult(num1, num2);
+
             } else if (operacao_digitada.equals("/")) {
-                System.out.print("Digite o primeiro número: ");
-                int num1 = leitor.nextInt();
-                System.out.print("Digite o segundo número: ");
-                int num2 = leitor.nextInt();
 
                 if (num2 == 0) {
                     System.out.println("Erro: Divisão por zero não é permitida.");
@@ -47,23 +37,19 @@ public class Calculadora {
                     System.out.print("O resultado da divisão de " + num1 + " e " + num2 + " é: ");
                     div(num1, num2);
                 }
+                
             } else if (operacao_digitada.equals("**")) {
-                System.out.print("Digite o primeiro numero: ");
-                int num1 = leitor.nextInt();
-                System.out.print("Digite o segundo numero: ");
-                int num2 = leitor.nextInt();
-
                 System.out.print("O resultado da potenciação de " + num1 + " e " + num2 + " é de : ");
                 pot(num1, num2);
             }
 
             System.out.print("Deseja realizar outra operação? (S/N): ");
-            continuar = leitor.next();
-            leitor.nextLine();
+            continuar = scanner.next();
+            scanner.nextLine();
 
-        } while (continuar.equalsIgnoreCase("S")); // Continuar se o usuário digitar 's' ou 'S'
+        } while (continuar.equalsIgnoreCase("S" )); // Continuar se o usuário digitar 's' ou 'S'
 
-        leitor.close(); // Fechar o scanner
+        scanner.close(); // Fechar o scanner
     }
 
     // FUNÇÕES OU MÉTODOS DO CÓDIGO
